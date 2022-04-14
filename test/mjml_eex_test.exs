b7fc6fba40381acbe97ec80ebe3fabe3dad693d7
Alex Koutmos <akoutmos@gmail.com>
2022-04-14 18:49:47 -0400

    More tests

--- test/mjml_eex_test.exs
+++ test/mjml_eex_test.exs
@@ -5,7 +5,11 @@ defmodule MjmlEExTest do
     use MjmlEEx, mjml_template: "test_templates/basic_template.mjml.eex"
   end
 
-  describe "BasicTemplate.render" do
+  defmodule ConditionalTemplate do
+    use MjmlEEx, mjml_template: "test_templates/conditional_template.mjml.eex"
+  end
+
+  describe "BasicTemplate.render/1" do
     test "should raise an error if no assigns are provided" do
       assert_raise ArgumentError, ~r/assign @call_to_action_text not available in template/, fn ->
         BasicTemplate.render([])
@@ -14,6 +18,13 @@ defmodule MjmlEExTest do
 
     test "should render the template and contain the proper text when passed assigns" do
       assert BasicTemplate.render(call_to_action_text: "Click me please!") =~ "Click me please!"
+    end
+  end
+
+  describe "ConditionalTemplate.render/1" do
+    test "should output the correct button depending on the assigns" do
+      assert ConditionalTemplate.render(all_caps: true) =~ "SIGN UP TODAY!!"
+      assert ConditionalTemplate.render(all_caps: false) =~ "Sign up today!"
     end
   end
 end

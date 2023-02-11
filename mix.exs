55616fb657a3e4d92daf82ff26e76f7782b4a084
Roberto Estrada <restrada@paridin.com>
2023-02-10 18:49:29 -0600

    relax phoenix_html dependency

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule MjmlEEx.MixProject do
     [
       # Production deps
       {:mjml, "~> 1.3.2"},
-      {:phoenix_html, "~> 3.2.0"},
+      {:phoenix_html, "~> 3.2"},
       {:telemetry, "~> 1.0"},
       {:erlexec, "~> 2.0", optional: true},
 
